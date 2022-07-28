808964fa1cd5e403077ceb147a3e68705aa289c6
Quan Nguyen <minhquan.nguyen.fr@gmail.com>
2022-07-28 10:34:48 +0200

    Update chart1 and chart2

--- src/chart1.py
+++ src/chart1.py
@@ -6,7 +6,7 @@ csv = pandas.read_csv(r'./data/data-moyen.csv',  sep=';')
 
 fig, ax1 = plt.subplots(figsize=(12,6))
 
-res = sns.barplot(x="dep", y="surface", data=csv, ax=ax1)
+res = sns.barplot(x="dep", y="surface", data=csv, ax=ax1, label="Surface")
 
 res.set_xlabel("Département", fontsize = 10)
 res.set_ylabel("Surface", fontsize = 10)
@@ -14,7 +14,8 @@ res.set_ylabel("Surface", fontsize = 10)
 
 ax2 = ax1.twinx()
 
-res = sns.lineplot(x="dep", y="total", data=csv, ax=ax2)
+res = sns.lineplot(x="dep", y="total", data=csv, ax=ax2, label="Total")
 
+res.set_title("Surface brulée comparé à total par départements")
 
 plt.show()
